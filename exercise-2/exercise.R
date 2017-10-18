49aa715b6a4b22a733507e344572ee0a0da7fc63
Sarah Park <parks96@uw.edu>
2017-10-18 12:00:08 -0700

    completing all the questions from exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,19 +2,43 @@
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(a, b) {
+  lengthA <- length(a)
+  lengthB <- length(b)
+  length <-  lengthA - lengthB
+  return(paste("The difference in lengths is", length))
+}
 
 # Pass two vectors of different length to your `CompareLength` function
+length <- CompareLength(c(1, 2, 3), c(2))
 
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
-
+DescribeDifference <- function(a, b) {
+  lengthA <- length(a)
+  lengthB <- length(b)
+  if(lengthA > lengthB) {
+    return(paste("Your first vector is longer by", lengthA - lengthB))
+  } else {
+    return(paste("Your second vector is longer by", lengthB - lengthA))
+  }
+}
 
 # Pass two vectors to your `DescribeDifference` function
-
+difference <- DescribeDifference(c(1, 2, 3), c(2))
 
 ### Bonus ###
 
 # Rewrite your `DescribeDifference` function to tell you the name of the vector which is longer
+DescribeDifference2 <- function(a, b) {
+  lengthA <- length(a)
+  lengthB <- length(b)
+  if(lengthA > lengthB) {
+    return(cat(a, "is longer."))
+  } else {
+    return(cat(b, "is longer."))
+  }
+}
+difference2 <- DescribeDifference2(c(1, 2, 3), c(2))
